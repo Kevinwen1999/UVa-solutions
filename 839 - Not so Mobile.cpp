ab000ebf5d8ffb35a4aa09707908fd441585f143
Kevinwen1999 <cybergodzilla1999@gmail.com>
2016-04-09 11:42:14 -0400

    Created Code

---
+++ 839 - Not so Mobile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+bool solve(int& W)
+{
+    int W1, W2 , D1, D2;
+    bool b1 = true, b2 = true;
+    cin>>W1>>D1>>W2>>D2;
+    if (!W1) b1 = solve(W1);
+    if (!W2) b2 = solve(W2);
+    W = W1 + W2;
+    return b1 && b2 && (W1*D1 == W2*D2);
+}
+int main()
+{
+    int T, W;
+    cin>>T;
+    while(T--)
+    {
+        if (solve(W))
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+        if (T)
+            cout<<"\n";
+    }
+}
