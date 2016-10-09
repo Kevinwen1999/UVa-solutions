f5568aa263725be76687ce117270ee749365b798
Kevin Wen <cybergodzilla1999@gmail.com>
2016-10-08 22:23:38 -0400

    Number Theory, Modified Sieve

---
+++ 11408 - Count DePrimes.cpp
@@ -0,0 +1,29 @@
+#include <bitset>
+#include <iostream>
+#include <vector>
+using namespace std;
+const int MAXN = 5000000 + 9;
+long long prime[MAXN];
+int a, b;
+long long sum[MAXN];
+int main()
+{
+    //memset(nDiffPM, 0, sizeof nDiffPM);
+    long long i, j;
+    for (i = 2; i < MAXN; i++)
+    {
+        sum [i] = sum[i-1];
+        if (!prime[i])
+        {
+            sum[i]++;
+            for (j = i+i; j < MAXN; j+=i)
+                prime[j]+=i;
+        }
+        else if (!prime[prime[i]]) sum[i]++;
+    }
+    while (scanf("%d", &a) && a)
+    {
+        scanf("%d", &b);
+        printf("%lld\n", sum[b] - sum[a-1]);
+    }
+}
