780897fe35c314ef84d432f0def0a427f982cbbe
Kevin Wen <cybergodzilla1999@gmail.com>
2017-10-15 22:57:19 -0400

    Update 673 - Parentheses Balance.cpp

--- 673 - Parentheses Balance.cpp
+++ 673 - Parentheses Balance.cpp
@@ -43,3 +43,52 @@ int main() {
         cout << (check(0, N - 1) ? "Yes\n" : "No\n");
     }
 }
+// Intented solution as follow, O(N)
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define MMS(x, y) memset((x), (y), sizeof(x))
+#define ll unsigned long long
+#define vi vector <int>
+#define pii pair <int, int>
+#define EPS 1e-9
+#define EPSS 1e-10
+
+#define MAXN 100010
+#define MAXL 5010
+#define SEGN 300030
+using namespace std;
+
+int N;
+char S[200];
+vector <int> bra, sqr;
+int main() {
+    int T;
+    cin >> T;
+    cin.getline(S, 200);
+    while (T--) {
+        cin.getline(S, 200);
+        N = strlen(S);
+        bool OK = true;
+        bra.clear();
+        sqr.clear();
+        for (int i = 0; i < N && OK; i++) {
+            if (S[i] == '(') {
+                bra.push_back(i);
+            } else if (S[i] == ')') {
+                if (bra.empty()) OK = false;
+                if (!sqr.empty() && (sqr.back() > bra.back())) OK = false;
+                bra.pop_back();
+            } else if (S[i] == '[') {
+                sqr.push_back(i);
+            } else if (S[i] == ']') {
+                if (sqr.empty()) OK = false;
+                if (!bra.empty() && (bra.back() > sqr.back())) OK = false;
+                sqr.pop_back();
+            }
+
+        }
+        cout << ((OK && bra.empty() && sqr.empty())? "Yes\n" : "No\n");
+    }
+}
+
